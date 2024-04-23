6899270e0edf0a1184eaa0110c41643f8f86c3f3
LindaGinto <lindamolouseph@gmail.com>
2024-04-23 22:01:43 +0530

    initial

---
+++ demo/demo/doctype/testing_main/testing_main.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, lindamolouseph@gmail.com and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Testing Main', {
+	// refresh: function(frm) {
+
+	// }
+});
